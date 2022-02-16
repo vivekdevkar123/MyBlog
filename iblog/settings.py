7f3a7421323bf416445906870ad5e1a1d5f47cdb
vivekdevkar123 <mrvivekdevkar123@gmail.com>
2022-02-17 00:37:31 +0530

    debugging

--- iblog/settings.py
+++ iblog/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-how3w4u!9bhtc+m_aekt#f@wc0%$wg6i+(9!x))3&k7nv&=&)8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','vivekdevkarblog.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
