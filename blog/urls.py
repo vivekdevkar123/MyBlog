bb203f148e65e7b039b7987f2fc59ec13ecb7e81
vivekdevkar <vivekdevkar@vivekdevkar.vivekdevkar>
2022-05-15 13:58:53 -0400

    adding login logout function

--- blog/urls.py
+++ blog/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path,include
-from .views import home,post,category,blogs,login,signup
+from .views import home,post,category,blogs,signup,loginPage,logoutUser,main
 
 
 
 urlpatterns = [
-    path('',home),
+    path('',home,name='home'),
     path('blogs/',blogs),
-    path('login',login),
-    path('signup',signup),
     path('blog/<slug:url>/', post),
     path('category/<slug:url>/',category),
+    path('signup',signup),
+    path('login',loginPage, name='login'),
+    path('logout/',logoutUser,name='logout'),
+    path('main',main,name='main'),
 ]
