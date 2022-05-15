0dc9adcf810098645ed5b4d29335bf2db595b63b
vivekdevkar <vivekdevkar@vivekdevkar.vivekdevkar>
2022-05-15 13:58:53 -0400

    adding login logout function

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,17 @@
 import email
+from email import message
+from urllib import response
+import django
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from blog.models import Post,Category
 from django.contrib.auth.models import User,auth
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateUserForm
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -15,118 +22,69 @@ def home(request):
         'posts':posts,
         'cats':cats
     }
-    return render(request,'index.html',data)
+    return render(request,'main.html',data)
 
 
+@login_required(login_url='login')
 def post(request, url):
     post = Post.objects.get(url = url)
     cats = Category.objects.all()
 
     return render(request, 'posts.html',{'post': post, 'cats': cats})
 
-
+@login_required(login_url='login')
 def category(request,url):
     cat = Category.objects.get(url = url)
     posts = Post.objects.filter(cat=cat)
     return render(request,'category.html',{'cat':cat, 'posts':posts})
 
-
+@login_required(login_url='login')
 def blogs(request):
     all_post = Post.objects.all()
     return render(request,'blogs.html',{'all_post':all_post})
-    
-def login(request):
-        if request.method== 'POST':
-            email = request.POST['email']
-            password = request.POST['password']
-
-            user = auth.authenticate(email = email,password=password)
-
-            if user is not None:
-                auth.login(request, user)
-                return redirect("/")
-            else:
-                messages.info(request,'invalid credentials')
-                return redirect('login')
-        
-        else:
-            return render(request,'login.html')
-
-
 
 def signup(request):
-
-    if (request.method == 'POST'):
-
-        email = request.POST('email')
-        password1 = request.POST('password1')
-        password2 = request.POST('password2')
-
-        if(password1 == password2):
-            if User.objects.filter(email=email).exists():
-                messages.info(request,'Email Taken')
-                return redirect('signup')
-            else:   
-                user = User.objects.create_user(password=password1, email=email)
-                user.save();
-                print('user created')
-                return redirect('login')
-            
-        else:
-            messages.info(request,'password not matching..')    
-            return redirect('signup')
-    else:
-        return render(request,'signup.html')
-
-
-'''
-
-def login(request):
-    if request.method== 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-
-        user = auth.authenticate(username=username,password=password)
-
-        if user is not None:
-            auth.login(request, user)
-            return redirect("/")
-        else:
-            messages.info(request,'invalid credentials')
-            return redirect('login')
-
-    else:
-        return render(request,'login.html')    
-
-def register(request):
+    form = CreateUserForm()
 
     if request.method == 'POST':
-        first_name = request.POST['first_name']
-        last_name = request.POST['last_name']
-        username = request.POST['username']
-        password1 = request.POST['password1']
-        password2 = request.POST['password2']
-        email = request.POST['email']
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request,'account is created for ' + user)
+            return redirect('login')
 
-        if password1==password2:
-            if User.objects.filter(username=username).exists():
-                messages.info(request,'Username Taken')
-                return redirect('register')
-            elif User.objects.filter(email=email).exists():
-                messages.info(request,'Email Taken')
-                return redirect('register')
-            else:   
-                user = User.objects.create_user(username=username, password=password1, email=email,first_name=first_name,last_name=last_name)
-                user.save();
-                print('user created')
-                return redirect('login')
+    context = {'form' : form}
+    return render(request,'signup.html',context)
 
+
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request,username=username,password=password)
+
+        if user is not None:
+            login(request,user)
+            return redirect('main')
         else:
-            messages.info(request,'password not matching..')    
-            return redirect('register')
-        return redirect('/')
-        
-    else:
-        return render(request,'register.html')
+            messages.info(request,'username or password is incorrect...')
 
-'''
+    return render(request,'login.html')
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
+
+@login_required(login_url='login')
+def main(request):
+    posts = Post.objects.all()[:11]
+    cats = Category.objects.all()
+    data = {
+        'posts':posts,
+        'cats':cats
+    }
+    return render(request,'index.html',data)
--- iblog/settings.py
+++ iblog/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog',
     'tinymce',
+
 ]
 
 MIDDLEWARE = [
@@ -129,3 +131,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
