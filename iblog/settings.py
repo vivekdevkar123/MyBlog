06b1968f54b6aed1993e1e318ff42c0a555e1b49
vivek devkar <mrvivekdevkar123@gmail.com>
2023-01-26 11:37:07 +0530

    debuge code

--- iblog/settings.py
+++ iblog/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-how3w4u!9bhtc+m_aekt#f@wc0%$wg6i+(9!x))3&k7nv&=&)8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','*']
 
