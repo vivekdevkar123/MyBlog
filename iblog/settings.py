c5ffae415a556d69431b30d9b6a8f1fe6c03bb54
vivek devkar <mrvivekdevkar123@gmail.com>
2023-02-07 20:28:58 +0530

    database changes

--- iblog/settings.py
+++ iblog/settings.py
@@ -67,13 +67,22 @@ WSGI_APPLICATION = 'iblog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': str(BASE_DIR / 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(BASE_DIR / 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'MyBlog',
+        'USER':'postgres',
+        'PASSWORD':'vivek',
+        'HOST':'localhost'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
